5451d9fd37a7b9790f60a3223ed3af7e9566da2a
Iffah Khoirul <iffah.khoirul@gmail.com>
2025-03-24 09:14:20 +0700

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -137,3 +137,13 @@ function printReceipt() {
         printWindow.close();
     };
 }
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("popup-overlay").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("popup-overlay").style.display = "none";
+}
